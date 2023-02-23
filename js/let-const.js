9cd89e8d7de7ebbd56f6048c791b531ac4d402fc
vatiza <rakibur.mac@gmail.com>
2023-02-24 00:53:21 +0600

    letconst

---
+++ js/let-const.js
@@ -0,0 +1,17 @@
+// brekup with var
+// no more use var
+// let: let it to reassign
+// const : do not allow it to reassign
+
+let money = 25;
+money = 35;
+console.log(money)
+const bird = 'jaan';
+// bird = 'rakin';
+// console.log(bird);
+const message = bird + 'pakhi';
+console.log(message);
+
+let numbers = [23, 32, 43, 12, 23];
+numbers = [33, 3232, 45, 3, 435];
+console.log(numbers)
