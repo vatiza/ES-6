a0a61786f34d80812dd6cccfb38b1ad234e59503
vatiza <rakibur.mac@gmail.com>
2023-02-24 01:35:47 +0600

    inter que in arrow.js file

---
+++ js/arrow.js
@@ -0,0 +1,17 @@
+//function declaration
+function add(first, second) {
+    const total = first + second;
+    return total;
+}
+
+
+const add1 = function add1(first, second) {
+    return total = first + second;
+
+}
+const add6 = (first, second) => first + second;
+
+const result = add6(10, 20);
+console.log(result);
+
+//! interview question: differences between function declaration, function expression  and arrow function
