2a810c75a87056492d8bc1135fdedd65fbba4cfb
vatiza <rakibur.mac@gmail.com>
2023-02-24 01:18:39 +0600

    temp

---
+++ js/template.js
@@ -0,0 +1,21 @@
+const person = 'adam sandler';
+const person2 = "ben WHite";
+const person3 = `Rakib`;
+const multiLine = 'first line \n' + 'second line code \n' + 'third line code';
+// console.log(multiLine);
+
+const newMultiLine = `First line code
+second line code
+thirst line 
+fournt line
+
+`;
+// 
+const a = 20;
+const b = 38;
+const nums = [34, 34, 234, 45, 32, 45, 6778, 2];
+const summary = 'sum of' + a + ' and ' + b + ' is ' + (a + b);
+// console.log(summary)
+const newSumary = `sum of ${a} and ${b} is : ${a + b}`
+const numbers = `total number ${nums}`;
+console.log(numbers);
